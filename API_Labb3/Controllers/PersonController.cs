3e7fbdaff8c01e3902279b15b520b80012cc71d1
theMargarita <margotvoistinova@hotmail.se>
2025-04-29 22:31:42 +0200

    added some seed adata and did not really finish the controllers, but half way there

---
+++ API_Labb3/Controllers/PersonController.cs
@@ -0,0 +1,64 @@
+﻿using API_Labb3.Data;
+using API_Labb3.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Labb3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly PiDbContext _context;
+
+        public PersonController(PiDbContext context)
+        {
+            _context = context;
+        }
+
+        //to be able to get all the people in person
+        [HttpGet (Name = "GetPerson")]
+        public async Task<ActionResult<ICollection<Person>>> GetPerson()
+        {
+            return Ok(await _context.Persons.ToListAsync());
+        }
+
+        //to be able to seach by their id
+        [HttpGet (Name = "GetPersonById")]
+        public async Task<ActionResult<ICollection<Person>>> GetPersonById(int id)
+        {
+            return Ok(await _context.Persons.FindAsync(id));
+        }
+
+        //to be able to create a new person 
+        [HttpPost(Name = "CreatePerson")]
+        public async Task<IActionResult> CreatePerson(Person newPerson)
+        {
+            if(newPerson == null)
+            {
+                return BadRequest(new { errorMessage = "Data is missing" });
+            }
+
+            var personToAdd = new Person()
+            {
+                Firstname = newPerson.Firstname,
+                Lastname = newPerson.Lastname,
+                Age = newPerson.Age,
+                Phone = newPerson.Phone
+            };
+
+            await _context.AddAsync(personToAdd);
+            return CreatedAtAction(nameof(GetPersonById), new { id = personToAdd.Id }, personToAdd);
+        }
+
+        //update part
+        //[HttpPut(Name = "UpdatePerson")]
+        //public async Task<IActionResult> UpdatePerson(int id, Person updatePerson)
+        //{
+        //    //var personToUpdate = await _context.Update(id);
+
+        //    if ()
+        //}
+    }
+}
--- API_Labb3/Data/PiDbContext.cs
+++ API_Labb3/Data/PiDbContext.cs
@@ -42,16 +42,33 @@ namespace API_Labb3.Data
                 .WithMany(pi => pi.Links)
                 .HasForeignKey(l => new { l.PerInt_ID, l.IntPer_ID }); //combine id to match the correct primary key 
 
+            SeedData(modelBuilder);
+        }
 
+        protected void SeedData(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasData(
+                new Person { Id = 1, Firstname = "Fibie", Lastname = "MeowMoew", Age = 16 },
+                new Person { Id = 2, Firstname = "Herman", Lastname = "Axelsson", Age = 27 },
+                new Person { Id = 3, Firstname = "Bob", Lastname = "Bobsson", Age = 78 },
+                new Person { Id = 4, Firstname = "Holly", Lastname = "WoofWoof", Age = 10 },
+                new Person { Id = 5, Firstname = "Rosa", Lastname = "FierceKitty", Age = 17 }
+                );
 
-            //modelBuilder.Entity<PersonIntreset>()
-            //    .HasMany(p => p.Persons)
-            //    .WithMany(i => i.Pers)
-            //    .UsingEntity(pi => pi.HasData(
-            //        ));
+            modelBuilder.Entity<Intrest>()
+                .HasData(
+                new Intrest { Id = 1, Title = "Volleyball"},
+                new Intrest { Id = 2, Title = "Photograph", Description = "Taking pictures with either an old or a new camera"},
+                new Intrest { Id = 3, Title = "Gaming", Description = "Both computer and vidio games"},
+                new Intrest { Id = 4, Title = "Chess", Description = "Back and white peices"},
+                new Intrest { Id = 5, Title = "Programming", Description = "All kind of programming"},
+                new Intrest { Id = 6, Title = "Painting", Description = "Specificly with oilpaint"},
+                new Intrest { Id = 7, Title = "Climing", Description = "Outside climbing on big and steep mountains"},
+                new Intrest { Id = 8, Title = "Hiking", Description = "Somewhere, anywhere"}
+                );
 
 
         }
-
     }
 }
