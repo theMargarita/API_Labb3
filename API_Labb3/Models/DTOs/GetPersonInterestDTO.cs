4dc3213cd287b73bce4cdae10781fe0578e4f55c
theMargarita <margotvoistinova@hotmail.se>
2025-05-21 09:58:42 +0200

    fixed this dto

--- API_Labb3/Models/DTOs/GetPersonInterestDTO.cs
+++ API_Labb3/Models/DTOs/GetPersonInterestDTO.cs
@@ -4,11 +4,13 @@ namespace API_Labb3.Models.DTOs
 {
     public class GetPersonInterestDTO
     {
-        public string FirstName { get; set; } = string.Empty;
+        public string FirstName { get; set; }
         public string? LastName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
 
-        public ICollection<InterestDTO> Interests { get; set; } = [];
-        public IEnumerable<LinkDTO> LinkPerson { get; set; }
+        public ICollection<LinkDTO> URL { get; set; } = [];
+        //public ICollection<InterestDTO> Interests { get; set; } = [];
 
     }
 }
